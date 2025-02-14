71bf78696b439dc8e270e8e18a0c0ef1bb372ed5
Quanf-dev <quanf,dev@gmail.com>
2025-02-14 15:36:04 +0700

    fix deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CardSection from "./CardSection/CardSection";
 import Slider_carSection from "./Slider_carSection/Slider_carSection";
-import IndexContent from "./Content/indexContent";
+import IndexContent from "./Content/IndexContent";
 import IndexJoin from "./JoinSection/IndexJoin";
 import Header from "@/components/Header/Header";
 export default function Home() {
